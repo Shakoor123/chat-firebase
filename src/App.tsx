f9a4e6b3199c5aa34a795341425aefc3bb63cdc5
shakoor123 <shakoorchachu2001@gmail.com>
2022-10-11 10:06:26 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from "./assets/react.svg";
+import React from "react";
+import "./App.scss";
+import Register from "./pages/Register/Register";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Register />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="registerWrapper">
+        <h1 className="title">Register</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
